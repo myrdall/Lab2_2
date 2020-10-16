57d11cddebcb8f739d50018384f8ce8bf9707296
myrdall <trivarson@gmail.com>
2020-10-16 15:46:43 +0700

    init unit test

---
+++ UnitTest1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:\Projects\Lab2_2\Lab2_2\functions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+				
+		}
+	};
+}
